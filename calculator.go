2dbb39f57d5714449fe3b75077bc015547dc92b5
Kirill Peshin <kirill_peshin@epam.com>
2021-09-10 09:56:56 +0300

    01

---
+++ calculator.go
@@ -0,0 +1,17 @@
+package calculator
+
+func add(x int, y int) int {
+	return x + y
+}
+
+func multiply(x int, y int) int {
+	return x * y
+}
+
+func subtraction(x int, y int) int {
+	return x - y
+}
+
+func divide(x int, y int) int {
+	return x / y
+}
